9b7bf83ae5ed2851ec63848d5480e36a4a77e14c
Georvic Tur <georvic.tur@gmail.com>
2025-03-24 01:22:54 +0100

    feat: preparing repo

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to metabase-flightsql-driver
+
+
--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject metabase-driver "0.1.0-SNAPSHOT"
+(defproject metabase-flightsql-driver "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
